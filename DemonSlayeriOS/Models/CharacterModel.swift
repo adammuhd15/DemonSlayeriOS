9197a3c4e9b6337140f0336106a86042227f74c4
adammuhd15 <adam_muhd96@hotmail.co.uk>
2025-06-01 23:59:54 +0800

    wip: Setup Project Structure

---
+++ DemonSlayeriOS/Models/CharacterModel.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterModel.swift
+//  DemonSlayeriOS
+//
+//  Created by Adam Muhammad on 01/06/2025.
+//
+
+import Foundation
---
+++ DemonSlayeriOS/ViewModels/CharacterViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterViewModel.swift
+//  DemonSlayeriOS
+//
+//  Created by Adam Muhammad on 01/06/2025.
+//
+
+import Foundation
