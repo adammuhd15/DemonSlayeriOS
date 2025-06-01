3d7301425bf7ac6cb5c3207780799e44a28f8698
adammuhd15 <adam_muhd96@hotmail.co.uk>
2025-06-01 23:40:37 +0800

    first commit

---
+++ DemonSlayeriOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemonSlayeriOS
+//
+//  Created by Adam Muhammad on 01/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Demon Slayer iOS")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemonSlayeriOS/DemonSlayeriOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemonSlayeriOSApp.swift
+//  DemonSlayeriOS
+//
+//  Created by Adam Muhammad on 01/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct DemonSlayeriOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
